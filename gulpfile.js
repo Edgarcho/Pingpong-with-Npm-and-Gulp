4d5d0d4c5e38eb2d65393a6486abb5af3ba1a762
Edgar Hernandez <edgarchortiz@gmail.com>
2018-02-05 22:58:31 -0800

    Added Moment.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,52 @@
+var del = require('del');
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 var gulp = require('gulp');
+
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
-var browserify = require('browserify');
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
-var source = require('vinyl-source-stream');
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+gulp.task("clean", function(){
+  return del(['build', 'tmp']);
+});
+gulp.task("build", ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+var lib = require('bower-files')();
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
